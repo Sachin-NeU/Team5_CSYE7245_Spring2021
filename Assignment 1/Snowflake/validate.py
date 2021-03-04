e4cd0366811ad93ca56dea7e39a88dc921d51534
Vivek Kulkarni <catchvivek94@gmail.com>
2021-03-04 18:55:37 +0530

    Snowflake python files

---
+++ Assignment 1/Snowflake/validate.py
@@ -0,0 +1,19 @@
+from snowflake.sqlalchemy import URL
+from sqlalchemy import create_engine
+import pandas as pd
+
+engine = create_engine(URL(
+    account = 'soa94276',
+    user = 'vivekkulkarni',
+    password = 'Siemens@1',
+    database = 'SEVIRDB',
+    schema = 'public',
+    warehouse = 'SEVIRDATA',
+    role='ACCOUNTADMIN'
+))
+
+
+connection = engine.connect()
+
+df = pd.read_sql_query("SELECT * FROM data3files", engine)
+print(df.head())
