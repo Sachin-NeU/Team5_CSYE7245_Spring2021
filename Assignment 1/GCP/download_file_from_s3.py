e0da3ecce2562043fc8f3c5d016c122b5519b422
SachinNeU <lnu.sac@husky.neu.edu>
2021-03-04 22:52:12 -0500

    'sachin'

--- Assignment 1/GCP/download_file_from_s3.py
+++ Assignment 1/GCP/download_file_from_s3.py
@@ -11,7 +11,7 @@ filename = 'CATALOG.csv'
 
 # Connect to Boto3
 #s3 = boto3.resource(service_name='s3',region_name='us-east-1')
-s3 = boto3.client('s3', aws_access_key_id='---' , aws_secret_access_key='---')
+s3 = boto3.client('s3', aws_access_key_id='AKIA4JTQMKMKJCBAUQPI' , aws_secret_access_key='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')
 s3.download_file(bucket_name,filename,filename)
 
 
