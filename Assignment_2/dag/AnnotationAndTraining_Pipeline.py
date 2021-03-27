de63919c53feb9261192de433f8073fea21636c6
Vivek Kulkarni <catchvivek94@gmail.com>
2021-03-26 20:39:30 -0400

    Assignment 2

--- Assignment_2/dag/AnnotationAndTraining_Pipeline.py
+++ Assignment_2/dag/AnnotationAndTraining_Pipeline.py
@@ -16,10 +16,16 @@ default_args = {
         'retry_delay': timedelta(minutes=5),
       }
 
-dag = DAG('inference', default_args=default_args)
+dag = DAG('datapreprocessing', default_args=default_args)
 
 t1 = BashOperator(
-    task_id='inference',
-    bash_command='python3 /home/user/.local/lib/python3.8/site-packages/airflow/pythonfiles/inference.py',
+    task_id='datapreprocessing',
+    bash_command='python3 /home/user/.local/lib/python3.8/site-packages/airflow/pythonfiles/dataprocessing.py',
     dag=dag)
     
+t2 = BashOperator(
+    task_id='comprehend',
+    bash_command='python3 /home/user/.local/lib/python3.8/site-packages/airflow/pythonfiles/comprehend.py',
+    dag=dag)
+    
+t1 >> t2
