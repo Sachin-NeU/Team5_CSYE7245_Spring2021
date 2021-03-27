30cc1e9019fc958181352626aec2f5da38bbfc82
Vivek Kulkarni <catchvivek94@gmail.com>
2021-03-26 20:09:13 -0400

    Assignment 2

--- Assignment_2/python/layer1DAG.py
+++ Assignment_2/python/layer1DAG.py
@@ -1,10 +1,11 @@
 from airflow import DAG
-from airflow.operators import BashOperator,PythonOperator
+from airflow.operators.bash import BashOperator
+#from airflow.operators import BashOperator,PythonOperator
 from datetime import datetime, timedelta
 
 seven_days_ago = datetime.combine(datetime.today() - timedelta(7),datetime.min.time())
 
- default_args = {
+default_args = {
         'owner': 'airflow',
         'depends_on_past': False,
         'start_date': seven_days_ago,
@@ -15,8 +16,16 @@ seven_days_ago = datetime.combine(datetime.today() - timedelta(7),datetime.min.t
         'retry_delay': timedelta(minutes=5),
       }
 
-dag = DAG('simple', default_args=default_args)
+dag = DAG('datapreprocessing', default_args=default_args)
+
 t1 = BashOperator(
-    task_id='testairflow',
-    bash_command='python /home/airflow/airflow/dags/scripts/file1.py',
+    task_id='datapreprocessing',
+    bash_command='python3 /home/user/.local/lib/python3.8/site-packages/airflow/pythonfiles/dataprocessing.py',
     dag=dag)
+    
+t2 = BashOperator(
+    task_id='comprehend',
+    bash_command='python3 /home/user/.local/lib/python3.8/site-packages/airflow/pythonfiles/comprehend.py',
+    dag=dag)
+    
+t1 >> t2
